6836533e07f0689e26401421920dcf063574f3ab
Dylan Schroeder <schrodac@gmail.com>
2018-06-19 15:31:37 -0500

    add results for vote count

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ function getNextImage() {
   return image;
 }
 
+var voteCount = 0;
 function showImages() {
+  if (voteCount >= 5) {
+    showResults();
+    return;
+  }
   //have image 1 show up on page
   var image1 = getNextImage();
   var img1 = document.getElementById('product-1');
@@ -29,27 +34,42 @@ function showImages() {
 var productImages = document.querySelectorAll('#voting img');
 for(var i = 0; i < productImages.length; i++) {
   productImages[i].addEventListener('click', function (event){
-    console.log('click', event.target.currentProduct);
+    voteCount++;
+
+    console.log('click #' + voteCount, event.target.currentProduct);
     showImages();
   } );
 }
 
 
 
-function Product(name, src) {
+function Product(name, src, testVoteCount) {
   this.name = name;
   this.src= src;
+  this.voteCount = testVoteCount || 0;
 
   Product.all.push(this);
 }
 Product.all = [];
 
-new Product('R2-D2 Bag', 'img/bag.jpg');
-new Product('Meat Gum', 'img/bubblegum.jpg');
-new Product('InstaBreakfast', 'img/breakfast.jpg');
-new Product('Toilet iPad', 'img/bathroom.jpg');
-new Product('Cthulhu Action Figure', 'img/cthulhu.jpg');
-new Product('Pet Sweepers', 'img/pet-sweep.jpg');
-new Product('Unicorn Meat', 'img/unicorn.jpg');
+new Product('R2-D2 Bag', 'img/bag.jpg', 2);
+new Product('Meat Gum', 'img/bubblegum.jpg', 4);
+new Product('InstaBreakfast', 'img/breakfast.jpg', 6);
+new Product('Toilet iPad', 'img/bathroom.jpg', 8);
+new Product('Cthulhu Action Figure', 'img/cthulhu.jpg', 1);
+new Product('Pet Sweepers', 'img/pet-sweep.jpg', 3);
+new Product('Unicorn Meat', 'img/unicorn.jpg', 5);
 
 window.addEventListener('load', showImages);
+
+function showResults() {
+  var ul = document.getElementById('results');
+  ul.innerHTML = '';
+
+  for(var i = 0; i < Product.all.length; i++) {
+    var current = Product.all[i];
+    var li = document.createElement('li');
+    li.textContent = current.name + ' got ' + current.voteCount + ' votes';
+    ul.appendChild(li);
+  }
+}
