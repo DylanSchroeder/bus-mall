bb25557bb2429f01aed0a2d385059f75ef959b6b
Dylan Schroeder <schrodac@gmail.com>
2018-06-19 15:12:27 -0500

    randomized first image after click

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 'use strict';
 
-var nextImage = 0;
+function getNextImage() {
+  var nextIndex = Math.floor(Math.random() * Product.all.length);
+  var image = Product.all[nextIndex];
+
+  return image;
+}
+
 function showImages() {
   //have image 1 show up on page
-  var image1 = Product.all[nextImage++];
+  var image1 = getNextImage();
   var img1 = document.getElementById('product-1');
   img1.src = image1.src;
   img1.currentProduct = image1;
 
-  var image2 = Product.all[nextImage++];
+  var image2 = getNextImage;
   var img2 = document.getElementById('product-2');
   img2.src = image2.src;
   img2.currentProduct = image2;
 
-  var image3 = Product.all[nextImage++];
+  var image3 = getNextImage;
   var img3 = document.getElementById('product-3');
   img3.src = image3.src;
   img3.currentProduct = image3;
